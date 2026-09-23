68ca1ba195c93ecd613c81a5fe397f3f4705ee00
harishjaved <mohammedharishjaved@gmail.com>
2026-09-23 17:57:08 +0530

    Update Facebook link

--- src/data/siteConfig.ts
+++ src/data/siteConfig.ts
@@ -9,7 +9,7 @@ export const siteConfig = {
   openingHours: "Mon-Sat: 9:00 AM - 7:00 PM | Sun: Closed",
   social: {
     instagram: "https://www.instagram.com/shine_bros__/",
-    facebook: "https://facebook.com/shinebros",
+    facebook: "https://www.facebook.com/p/Shine-Bros-Auto-Spa-61588641463879/",
     youtube: "https://youtube.com/shinebros",
   },
   googleMapsUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111111!2d77.1111!3d28.1111!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDA2JzQwLjAiTiA3N8KwMDYnNDAuMCJF!5e0!3m2!1sen!2sin!4v1234567890123!5m2!1sen!2sin",
